c90b9a365c886644e3910cd668660796fd2a2f5e
amandadhwal <amandadhwal911@gmail.com>
2024-09-30 21:46:12 +0530

    project complete

---
+++ update_page_1.php
@@ -0,0 +1,67 @@
+<?php
+include ('header.php');
+include ('dbcon.php');
+
+if(isset($_GET['id']))
+{
+    $id = $_GET['id'];
+    $query = "select * from `students` where `id`='$id'";
+
+    $result =mysqli_query($connection, $query);
+
+    if(!$result)
+    {
+        die("query failed".mysqli_error);
+    }
+    else
+    {
+        $row = mysqli_fetch_assoc($result);
+    }
+}
+
+if(isset($_POST['update_student']))
+{
+    if(isset($_GET['id_new']))
+    {
+        $idnew = $_GET['id_new'];
+    }
+
+        $fname = $_POST['f_name'];
+        $lname = $_POST['l_name'];
+        $age = $_POST['age'];
+
+        $query = "update `students` set `first_name`='$fname', `last_name`='$lname', `age`='$age' where `id`='$idnew'";
+
+        $result = mysqli_query($connection, $query);
+        if(!$result)
+        {
+            die("query failed".mysqli_error);
+        }
+        else
+        {
+            header('location:index.php?update_msg=Details Updated Successfully');
+        }
+}
+
+?>
+<form action="update_page_1.php?id_new=<?php echo $id; ?>" method="post">
+            <div class="form-group">
+                <label for="f_name">First Name</label>
+                <input type="text" name="f_name" class="form-control" value="<?php echo $row['first_name'] ?>" >
+            </div>
+            <div class="form-group">
+                <label for="l_name">Last Name</label>
+                <input type="text" name="l_name" class="form-control" value="<?php echo $row['last_name'] ?>">
+            </div>
+            <div class="form-group">
+                <label for="age">Age</label>
+                <input type="text" name="age" class="form-control" value="<?php echo $row['age'] ?>">
+            </div>
+
+            <input type="submit" class="btn btn-primary" name="update_student" value="UPDATE">
+
+        </form>
+
+<?php
+include ('footer.php');
+?>
